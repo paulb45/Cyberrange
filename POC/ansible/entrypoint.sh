b8b06e1822051076034902159819cec94d42048d
paul <p.baratinus@gmail.com>
2024-11-09 18:30:22 +0100

    connexion ssh ok

--- POC/ansible/entrypoint.sh
+++ POC/ansible/entrypoint.sh
@@ -1,12 +1,17 @@
 #!/bin/sh
 
 mkdir -p /home/ansible/.ssh
-chown ansible:ansible /home/ansible/.ssh
-chmod 700 /home/ansible/.ssh
 
 HOME=/home/ansible
-ssh-keygen -t ed25519 -f ~/.ssh/id_ansible -N ''
+ssh-keygen -t ed25519 -f ~/.ssh/id_ed25519 -N ''
 
-cp /home/ansible/.ssh/id_ansible.pub /tmp/ssh/
+cp /home/ansible/.ssh/id_ed25519.pub /tmp/ssh/
+
+chown -R ansible:ansible /home/ansible/.ssh
+chmod -R 700 /home/ansible/.ssh
+
+# partie ssh-agent
+# su-exec ansible sh -c 'eval $(ssh-agent); ssh-add /home/ansible/.ssh/id_ansible'
 
 exec tail -f /dev/null
+
--- POC/serveur-alpine/entrypoint.sh
+++ POC/serveur-alpine/entrypoint.sh
@@ -2,12 +2,12 @@
 ssh-keygen -A
 
 # récupération de la clé publique du serveur ansible
-while [ ! -f /tmp/ssh/id_ansible.pub ]; do
+while [ ! -f /tmp/ssh/id_ed25519.pub ]; do
 	sleep 1
 done
 
 mkdir -p /home/ansible/.ssh
-cp /tmp/ssh/id_ansible.pub /home/ansible/.ssh/authorized_keys
+cp /tmp/ssh/id_ed25519.pub /home/ansible/.ssh/authorized_keys
 chown -R ansible:ansible /home/ansible/.ssh
 chmod -R 700 /home/ansible/.ssh
 
