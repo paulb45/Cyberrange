e44104b8aae74e062c8216533be02ba26a09d06b
paul <p.baratinus@gmail.com>
2025-01-10 19:54:48 +0100

    os debian pour les clients

---
+++ docker-images/debian-client/entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+sudo ssh-keygen -A
+
+# récupération de la clé publique du serveur ansible
+while [ ! -f /tmp/ssh/id_ed25519.pub ]; do
+	sleep 1
+done
+
+mkdir -p /home/ansible/.ssh
+cp /tmp/ssh/id_ed25519.pub /home/ansible/.ssh/authorized_keys
+chown -R ansible:ansible /home/ansible/.ssh
+chmod -R 700 /home/ansible/.ssh
+
+exec tail -f /dev/null
